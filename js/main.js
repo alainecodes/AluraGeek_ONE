a06a4cc37c099d672f0bbfa87e7a74042b340940
Alaine <137127353+alainecodes@users.noreply.github.com>
2023-10-03 21:10:58 -0300

    updating main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 export function fetchProdutos() {
-    return fetch('https://run.mocky.io/v3/f5d731bc-78a2-4c70-9643-988057eb46cc')
+    return fetch('/db.json')
         .then(response => response.json())
         .then(produtos => {
-            localStorage.setItem('produtos', JSON.stringify(produtos));
-            return produtos;
+            let categorias = produtos.reduce((acc, produto) => {
+                if (!acc[produto.categoria]) {
+                    acc[produto.categoria] = [];
+                }
+                acc[produto.categoria].push(produto);
+                return acc;
+            }, {});
+            localStorage.setItem('produtos', JSON.stringify(categorias));
+            return categorias;
         });
 }
 
-fetchProdutos().then(produtos => {
+fetchProdutos().then(categorias => {
 
-    for (let categoria in produtos) {
-        let section = document.getElementById(categoria);
-        let lista = document.createElement('ul');
-        lista.className = 'galeria__produtos-lista'
+    for (let categoria in categorias) {
 
-        produtos[categoria].forEach(item => {
+        let section = document.querySelector(`section#${categoria.replace(/[^a-zA-Z0-9-]/g, '')}`);
+        if (!section) continue;
+
+        let lista = section.querySelector('ul.galeria__produtos-lista');
+        if (!lista) continue;
+
+        lista.innerHTML = '';
+
+        categorias[categoria].forEach(item => {
 
             let listItem = document.createElement('li');
-            item.className = 'galeria__produtos-item'
+            listItem.className = 'galeria__produtos-item'
 
             let link = document.createElement('a');
             link.href = `./pages/produto.html?id=${item.id}`;
@@ -27,8 +39,7 @@ fetchProdutos().then(produtos => {
                             <img src="${item.imagemURL}">
                             <p class="galeria__produtos-item-titulo">${item.titulo}</p>
                             <p class="galeria__produtos-item-preco">R$${item.preco}</p>
-                            Ver produto
-                            
+                            Ver produto                          
                     </div>`;
 
             link.addEventListener('click', function (event) {
@@ -40,6 +51,5 @@ fetchProdutos().then(produtos => {
             listItem.appendChild(link);
             lista.appendChild(listItem);
         });
-        section.appendChild(lista);
     }
 });
